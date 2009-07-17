c4728eb7f06d2cce1f37d0f98adc30de51a01af6
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2009-07-17 06:53:23 +0000

    new tomcat snapshot
    git-svn-id: https://svn.codehaus.org/grails/trunk/grails-plugins/grails-tomcat/trunk@9020 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,4 +1,6 @@
 // No programmable web.xml path yet, so put it in the right place automatically
 eventGenerateWebXmlEnd = {
+	
+	ant.copy(file:"${grailsSettings.projectWorkDir}/resources/web.xml", todir:"${basedir}/web-app/WEB-INF") 
 	System.setProperty("grails.server.factory", "org.grails.tomcat.TomcatServerFactory")
 }
