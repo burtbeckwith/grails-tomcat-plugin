72e289e5a78d8f17ae97a7407e8a99954c0f90aa
Jeff Brown <jeff@jeffandbetsy.net>
2010-05-17 09:19:04 -0500

    1.3.2.BUILD-SNAPSHOT

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  */
 class TomcatGrailsPlugin {
     // the plugin version
-    def version = "1.3.1"
+    def version = "1.3.2.BUILD-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // resources that are excluded from plugin packaging
