8a993794e35c08a7e65583bdea086802dfbb8e0d
Jeff Brown <jeff@jeffandbetsy.net>
2010-07-08 07:03:22 -0500

    set version number to 1.3.3

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  */
 class TomcatGrailsPlugin {
     // the plugin version
-    def version = "1.3.3.BUILD-SNAPSHOT"
+    def version = "1.3.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // resources that are excluded from plugin packaging
