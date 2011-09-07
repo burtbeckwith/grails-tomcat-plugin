fcb67251b6432415944853368c1c106356e00fb1
Graeme Rocher <graemerocher@Graeme-Rochers-iMac.local>
2011-09-07 15:34:52 +0200

    updated version to 2.0.0.M2

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  */
 class TomcatGrailsPlugin {
     // the plugin version
-    def version = "2.0.0.M1"
+    def version = "2.0.0.M2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
