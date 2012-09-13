278b14cb3116a3c6be1774919f1852d89fc47748
Burt Beckwith <beckwithb@vmware.com>
2012-09-13 09:37:32 -0400

    metadata update

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -21,4 +21,9 @@ class TomcatGrailsPlugin {
     def title = "Apache Tomcat plugin for Grails"
     def description = 'Makes Tomcat 7.0 the default servlet container for Grails at development time.'
     def documentation = "http://grails.org/plugin/tomcat"
+
+    def license = 'APACHE'
+    def organization = [name: 'SpringSource', url: 'http://www.springsource.org/']
+    def issueManagement = [system: 'JIRA', url: 'http://jira.grails.org/browse/GPTOMCAT']
+    def scm = [url: 'https://github.com/grails-plugins/grails-tomcat-plugin']
 }
