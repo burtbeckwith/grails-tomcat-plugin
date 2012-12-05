b88407aae5d6c73f46570b8eb0fe566dbc00fa54
Graeme Rocher <graeme.rocher@gmail.com>
2012-12-05 14:12:36 +0100

    update version to 2.2.0 RC3

--- TomcatGrailsPlugin.groovy
+++ TomcatGrailsPlugin.groovy
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 class TomcatGrailsPlugin {
-    def version = "2.2.0.RC2"
+    def version = "2.2.0.RC3"
     def grailsVersion = "2.2 > *"
     def scopes = [excludes: 'war']
     def author = "Graeme Rocher"
