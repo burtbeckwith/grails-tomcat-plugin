acb63d0e7a7f4729ca296dee1b11eba696497337
Graeme Rocher <graeme.rocher@gmail.com>
2013-06-05 09:29:54 +0200

    fix exclusions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,16 +16,15 @@ grails.project.dependency.resolution = {
         String tomcatVersion = "7.0.40"
 
         runtime("org.apache.tomcat:tomcat-catalina-ant:$tomcatVersion") {
-            excludes 'tomcat-catalina', 'tomcat-coyote'
+            excludes 'org.apache.tomcat:tomcat-catalina', 'org.apache.tomcat:tomcat-coyote'
         }
         compile "org.apache.tomcat.embed:tomcat-embed-core:$tomcatVersion", {
-            excludes 'tomcat-embed-logging-juli', 'tomcat-embed-logging-log4j'
+            excludes 'org.apache.tomcat.embed:tomcat-embed-logging-juli', 'org.apache.tomcat.embed:tomcat-embed-logging-log4j'
         }
         runtime "org.apache.tomcat.embed:tomcat-embed-jasper:$tomcatVersion", {
-            excludes 'ecj', 'tomcat-embed-core'
+            excludes 'org.eclipse.jdt.core.compiler:ecj', 'org.apache.tomcat.embed:tomcat-embed-core'
         }
         runtime "org.apache.tomcat.embed:tomcat-embed-logging-log4j:$tomcatVersion"
-        runtime "org.apache.tomcat.embed:tomcat-embed-logging-juli:$tomcatVersion"
 
         // needed for JSP compilation
         runtime "org.eclipse.jdt.core.compiler:ecj:3.7.2"
