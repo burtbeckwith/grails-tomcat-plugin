d1f804a9281b62e13c255acbf5c44a5f21de3b10
Burt Beckwith <burt@burtbeckwith.com>
2013-10-15 11:42:32 -0400

    cleanup, whitespace

--- scripts/Tomcat.groovy
+++ scripts/Tomcat.groovy
@@ -8,7 +8,7 @@ ant.taskdef(name: "deploy",   classname: "org.apache.catalina.ant.DeployTask")
 ant.taskdef(name: "list",     classname: "org.apache.catalina.ant.ListTask")
 ant.taskdef(name: "undeploy", classname: "org.apache.catalina.ant.UndeployTask")
 
-target(main: '''\
+target(tomcat: '''\
 Script used to interact with remote Tomcat. The following subcommands are available:
 
 grails tomcat deploy - Deploy to a tomcat server
@@ -38,15 +38,14 @@ grails tomcat undeploy - Undeploy from a tomcat server
             configureServerContextPath()
             println "Undeploying application $serverContextPath from Tomcat"
             println '''\
-NOTE: If you experience a classloading error during undeployment you need to take the following steps:
+NOTE: If you experience a classloading error during undeployment you need to take the following step:
 
-* Upgrade to Tomcat 6.0.20 or above
 * Pass this system argument to Tomcat: -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false
 
-See http://tomcat.apache.org/tomcat-6.0-doc/config/systemprops.html for more information
+See http://tomcat.apache.org/tomcat-7.0-doc/config/systemprops.html for more information
 '''
             undeploy(url: url, path: serverContextPath, username: user, password: pass)
     }
 }
 
-setDefaultTarget "main"
+setDefaultTarget "tomcat"
