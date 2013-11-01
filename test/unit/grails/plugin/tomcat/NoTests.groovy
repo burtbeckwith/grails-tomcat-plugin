19cd3e51334f969f149466c2e8e76f89b2d20a84
Burt Beckwith <burt@burtbeckwith.com>
2013-10-31 22:13:44 -0400

    dummy test case to keep the build from failing because it finds no test results

---
+++ test/unit/grails/plugin/tomcat/NoTests.groovy
@@ -0,0 +1,7 @@
+package grails.plugin.tomcat
+
+// dummy test case to keep the build from failing because it finds no test results
+class NoTests extends GroovyTestCase {
+
+    void testNothing() {}
+}
