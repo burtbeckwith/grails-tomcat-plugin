845eabd7d451a4e0a154cc0360ae09318c44ac92
Burt Beckwith <burt@burtbeckwith.com>
2015-05-12 01:16:26 -0400

    8.0.22 release

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,28 +18,36 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		String tomcatVersion = '8.0.21'
+		String tomcatVersion = '8.0.22'
 
-		compile "org.apache.tomcat.embed:tomcat-embed-core:$tomcatVersion"
+		compile "org.apache.tomcat.embed:tomcat-embed-core:$tomcatVersion", {
+			excludes 'tomcat-embed-logging-juli', 'tomcat-embed-logging-log4j'
+		}
 
 		runtime "org.apache.tomcat.embed:tomcat-embed-el:$tomcatVersion"
 
-		runtime "org.apache.tomcat.embed:tomcat-embed-jasper:$tomcatVersion"
+		runtime "org.apache.tomcat.embed:tomcat-embed-jasper:$tomcatVersion", {
+			excludes 'ecj', 'tomcat-embed-core', 'tomcat-embed-el'
+		}
 
 		runtime "org.apache.tomcat.embed:tomcat-embed-logging-log4j:$tomcatVersion"
 
 		runtime "org.apache.tomcat.embed:tomcat-embed-logging-juli:$tomcatVersion"
 
-		runtime "org.apache.tomcat.embed:tomcat-embed-websocket:$tomcatVersion"
+		runtime "org.apache.tomcat.embed:tomcat-embed-websocket:$tomcatVersion", {
+			excludes 'tomcat-embed-core'
+		}
 
 		compile 'javax.servlet:javax.servlet-api:3.1.0'
 
 		// needed for JSP compilation
-		runtime 'org.eclipse.jdt.core.compiler:ecj:4.4'
+		runtime 'org.eclipse.jdt.core.compiler:ecj:4.4.2', {
+			excludes 'ant'
+		}
 	}
 
 	plugins {
-		build ':release:3.0.1', ':rest-client-builder:2.0.3', {
+		build ':release:3.1.1', ':rest-client-builder:2.1.1', {
 			export = false
 		}
 	}
